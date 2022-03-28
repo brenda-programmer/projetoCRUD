2f0b7d3fd8313955a3b61b821c492b336d88cf9d
Brenda Oliveira <brenda.programmer@gmail.com>
2022-03-27 21:35:47 -0300

    Criado o projeto CRUD

---
+++ DAL/Model/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    public class Aluno
+    {
+        //Declaração dos atributos da classe Aluno
+        public int Matricula { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Email { get; set; }
+
+    }
+}
